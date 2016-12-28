608cecbd86b75d211255a0c5f41e60688a9d02e7
Takuto Kimura <takuto@heart-s.com>
2016-12-28 16:17:48 +0900

    update sample program

--- workspace/test/main.c
+++ workspace/test/main.c
@@ -1,10 +1,19 @@
 #include <gba.h>
 
 int AgbMain(){
+  u16 i;
   u16 *bg = VideoBuffer;
   SetMode( MODE_3 | BG2_ENABLE );
   
-  bg[0] = RGB(31, 31, 31);
+  for(i=0; i<240*50; i++){
+     bg[i] = RGB(31, 0, 0);
+  }
+  for(; i<240*100; i++){
+     bg[i] = RGB( 0,31, 0);
+  }
+  for(; i<240*160; i++){
+     bg[i] = RGB( 0, 0,31);
+  }
   
   return 1;
 }
